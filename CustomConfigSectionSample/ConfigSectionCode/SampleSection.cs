73cca8eaae03dfb8a5cf5f96cbd45eca1f9d1cfd
Jeremy <muqluk@yahoo.com>
2017-09-19 09:21:27 -0500

    Initial Commit

---
+++ CustomConfigSectionSample/ConfigSectionCode/SampleSection.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Configuration;
+
+namespace ConfigSectionCode
+{
+    public class UrlsSection : ConfigurationSection
+    {
+        [ConfigurationProperty("urls", IsDefaultCollection = false)]
+        [ConfigurationCollection(typeof(UrlsCollection), AddItemName = "add", ClearItemsName = "clear", RemoveItemName = "remove")]
+        public UrlsCollection Urls
+        {
+            get
+            {
+                return (UrlsCollection)base["urls"];
+            }
+
+        }
+        
+        //public UrlsSection()
+        //{
+        //    Urls.Add(new UrlConfigElement());
+        //}
+
+    }
+    
+    public class UrlsCollection : ConfigurationElementCollection
+    {
+        public UrlsCollection() { }
+
+        public override ConfigurationElementCollectionType CollectionType
+        {
+            get
+            {
+                return ConfigurationElementCollectionType.AddRemoveClearMap;
+            }
+        }
+
+        protected override ConfigurationElement CreateNewElement()
+        {
+            return new UrlConfigElement();
+        }
+
+        protected override Object GetElementKey(ConfigurationElement element)
+        {
+            return ((UrlConfigElement)element).Name;
+        }
+
+        public UrlConfigElement this[int index]
+        {
+            get
+            {
+                return (UrlConfigElement)BaseGet(index);
+            }
+        }
+
+        new public UrlConfigElement this[string Name]
+        {
+            get
+            {
+                return (UrlConfigElement)BaseGet(Name);
+            }
+        }
+
+
+        public int IndexOf(UrlConfigElement url)
+        {
+            return BaseIndexOf(url);
+        }
+
+        //public void Add(UrlConfigElement url)
+        //{
+        //    BaseAdd(url);
+        //}
+
+        //protected override void BaseAdd(ConfigurationElement element)
+        //{
+        //    BaseAdd(element, false);
+        //}
+    }
+    
+    public class UrlConfigElement : ConfigurationElement
+    {
+        //public UrlConfigElement(String name, String url, int port)
+        //{
+        //    this.Name = name;
+        //    this.Url = url;
+        //    this.Port = port;
+        //}
+
+        public UrlConfigElement() { }
+
+        [ConfigurationProperty("name", DefaultValue = "Contoso", IsRequired = true, IsKey = true)]
+        public string Name
+        {
+            get
+            {
+                return (string)this["name"];
+            }
+            //set
+            //{
+            //    this["name"] = value;
+            //}
+        }
+
+        [ConfigurationProperty("url", DefaultValue = "http://www.contoso.com", IsRequired = true)]
+        [RegexStringValidator(@"\w+:\/\/[\w.]+\S*")]
+        public string Url
+        {
+            get
+            {
+                return (string)this["url"];
+            }
+            //set
+            //{
+            //    this["url"] = value;
+            //}
+        }
+
+        [ConfigurationProperty("port", DefaultValue = (int)4040, IsRequired = false)]
+        [IntegerValidator(MinValue = 0, MaxValue = 8080, ExcludeRange = false)]
+        public int Port
+        {
+            get
+            {
+                return (int)this["port"];
+            }
+            //set
+            //{
+            //    this["port"] = value;
+            //}
+        }
+
+    }
+
+}
---
+++ CustomConfigSectionSample/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Windows;
+
+using ConfigSectionCode;
+
+namespace CustomConfigSectionSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetEnvironments();
+        }
+
+        public void GetEnvironments()
+        {
+            string output;
+            Configuration envConfig = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None) as Configuration;
+
+            EnvironmentConfigSection environmentsSection = envConfig.GetSection("ConfiguredEnvironments") as EnvironmentConfigSection;
+
+            if (environmentsSection != null)
+            {
+                List<EnvironmentElement> environments = (from EnvironmentElement env in environmentsSection.Environments select env).ToList();
+                MessageBox.Show(environments.Select(x => x.Name).First());
+            }
+            else
+            {
+                throw new System.Exception("Issues with your config idiot.");
+            }
+
+        }
+
+        private void GetConfig1()
+        {
+            string output;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None) as Configuration;
+            UrlsSection myUrlsSection = config.GetSection("MyUrls") as UrlsSection;
+
+            if (myUrlsSection == null)
+            {
+                output = "Failed to load UrlsSection.";
+            }
+            else
+            {
+                output = "Collection elements contained in the custom section collection:\r\n";
+                for (int i = 0; i < myUrlsSection.Urls.Count; i++)
+                {
+                    output += $"   Name={myUrlsSection.Urls[i].Name} URL={myUrlsSection.Urls[i].Url} Port={myUrlsSection.Urls[i].Port}\r\n";
+                }
+            }
+
+        }
+    }
+}
